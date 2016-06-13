0454ff06e8f346c3fc4f5833bf4a1ae32cdd7856
Pluto Yang <yangyj.ee@gmail.com>
2016-06-13 20:16:53 +0800

    Make rayleigh display real pdf

--- rayleigh/ray.cpp
+++ rayleigh/ray.cpp
@@ -6,20 +6,23 @@
 
 using namespace std;
 using namespace itpp;
-const int LEN = 10000;
+const int LEN = 100000;
+const double step = 0.1;
 
 int main(int argc, char* argv[])
 {
     Independent_Fading_Generator fad1;
     fad1.set_LOS_power(0);  // no LOS, so it's rayleigh
     cvec tc;
+    RNG_randomize();
     fad1.generate(LEN,tc);
-    Histogram<double> hist(0, 3, 21);
+    Histogram<double> hist(step / 2, step * 30 - step / 2, 30);
     hist.update(abs(tc));
     vec pdf = hist.get_pdf();
+    pdf = pdf / step;
     ofstream f("test.dat");
     for (int i = 0; i < pdf.length(); ++i) {
-        f << setw(4) << hist.get_bin_center(i) << setw(12) << pdf(i) << endl;
+        f << setw(4) << hist.get_bin_right(i) << setw(12) << pdf(i) << endl;
     }
     f.close();
     return 0;
--- rayleigh/test.gp
+++ rayleigh/test.gp
@@ -2,5 +2,6 @@ set grid
 set xlabel "Fading envelop"
 set ylabel "PDF"
 set title "PDF of rayleigh fading"
+set yrange [0:1]
 plot "test.dat" using 1:2 w lp pt 5
 pause mouse any "Hit any key to exit\n\r"
