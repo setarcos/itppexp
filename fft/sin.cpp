1317aabeccd741dc72bfe650598722b581aa9fb7
YanJun Yang <yangyj.ee@gmail.com>
2017-04-21 10:38:19 +0800

    Upsample the signal makes frequency domain repeat

--- fft/sin.cpp
+++ fft/sin.cpp
@@ -8,19 +8,31 @@ const double PI=3.1415926;
 const int fs = 500;
 const int T = 1;
 
-int main()
+int main(int argc, char * argv[])
 {
     vec b;
     int f1 = 17;
     int f2 = 50;
     int N = T * fs;  // frequency resolution is fs/N=1/T
     b.set_size(N);
+    unsigned char t = 0;
+    if (argc >= 2)
+        t = atoi(argv[1]);
 
     for (int i = 0; i < N; ++i) {
         b[i] = 0.5 + sin(2 * PI * (f1 + 0.3)/ fs *i) + sin(2 * PI * f2 / fs * i);
     }
+    vec d;
+    switch (t) {
+        case 1:
+            d = upsample(b, 4);
+            N *= 4;
+            break;
+        default:
+            d = b;
+    }
+    cvec c = fft_real(d);
     ofstream ofs ("test.dat", ofstream::out);
-    cvec c = fft_real(b);
     for (int i = 0; i < N; ++i) {
         ofs << abs(c[i]) / N << endl;
     }
