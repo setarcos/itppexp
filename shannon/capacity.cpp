642bbfdd6d50154d26040f756f346dfea37ce556
Pluto Yang <yangyj.ee@gmail.com>
2021-05-20 18:47:23 +0800

    Add SNR vs Pe

--- shannon/capacity.cpp
+++ shannon/capacity.cpp
@@ -34,13 +34,14 @@ int main(int argc, char* argv[])
                 f << setw(4) << snrdb(i) << setw(12) << 10 * log10(snr[i] / rho(snr[i])) << endl;
         }
     }
-    if (t == 2) {
+    if (t >= 2) {
         vec pe = linspace(-7, -1, 13);
         vec pel = pow(10, pe);
         vec r = "0.01, 0.5, 0.75, 2.0";
         for (int i = 0; i < pe.length(); ++i) {
             for (int j = 0; j < r.length(); ++j) {
-                double ebnol = (pow(2, (r[j] * (1 - h2p(pel[i])))) - 1) / r[j];
+                double ebnol = (pow(2, (r[j] * (1 - h2p(pel[i])))) - 1); // Actually SNR
+                if (t == 2) ebnol /= r[j];
                 f << setw(12) << 10 * log10(ebnol);
             }
             f << setw(12) << pe[i] << endl;
@@ -49,7 +50,7 @@ int main(int argc, char* argv[])
     f.close();
     f.open("test.gp");
     f << "set grid" << endl;
-    if (t < 2)
+    if ((t < 2) || (t == 3))
         f << "set xlabel \"SNR[db]\"" << endl;
     else
         f << "set xlabel \"EbN0[db]\"" << endl;
@@ -57,7 +58,7 @@ int main(int argc, char* argv[])
         f << "set ylabel \"Rho\"" << endl;
     if (t == 1)
         f << "set ylabel \"EbN0[db]\"" << endl;
-    if (t == 2)
+    if (t >= 2)
         f << "set ylabel \"Pe[log10]\"" << endl;
     if (t < 2) {
         f << "set xrange [-20:20]" << endl;
@@ -65,7 +66,10 @@ int main(int argc, char* argv[])
         if (t == 0) f << "title \"Rho vs SNR\"" << endl;
         else f << "title \"EbN0 vs SNR\"" << endl;
     } else {
-        f << "set xrange [-5:3]" << endl;
+        if (t == 2)
+            f << "set xrange [-5:3]" << endl;
+        else
+            f << "set xrange [-25:5]" << endl;
         f << "plot \"test.dat\" using 1:5 w lp pt 5 title \"R=0.01\",\\" << endl;
         f << "    \"test.dat\" using 2:5 w lp pt 4 title \"R=0.5\",\\" << endl;
         f << "    \"test.dat\" using 3:5 w lp pt 3 title \"R=0.75\",\\" << endl;
