48decab5b2d77f75f1d7ff26c64da5a46dead476
Pluto Yang <yangyj.ee@gmail.com>
2022-02-13 19:54:08 +0800

    Plot BER/FER of Polar code

--- polar/main.cpp
+++ polar/main.cpp
@@ -12,49 +12,67 @@ using std::setw;
 #define BLK_LEN 2048
 #define CRC "CCITT-16"
 #define CRC_LEN 16
+#define LIST_SIZE 16
 
 int main(int argc, char *argv[])
 {
     Polar p(BLK_LEN, BLK_LEN / 2);
-    BERC ber;
     p.gen_frozen_bec(0.32);
-
+    BERC ber;
+    BLERC bler;
     vec EbN0db = "1:0.25:3";
-    vec bit_error_rate;
+    vec bit_erate;
+    vec blk_erate;
     int length = EbN0db.length();
-    bit_error_rate.set_size(length * 2);
+    bit_erate.set_size(length);
+    blk_erate.set_size(length);
     RNG_randomize();
-    for (int m = 0; m < 2; ++m) {
-        bvec bitsin;
-        if (m == 0) {
-            p.set_polar_decoder(POLAR_SC);
-            bitsin = randb(BLK_LEN * 1000);
-        }
-        if (m == 1) {
-            p.set_polar_decoder(POLAR_CASCL);
-            p.set_crc_code(CRC, CRC_LEN);
-            p.set_scl_size(8);
-            bitsin = randb((BLK_LEN - CRC_LEN) * 1000);
-        }
-        for (int i = 0; i < length; ++i) {
-            double N0 = pow(10.0, -EbN0db[i] / 10.0) / p.get_rate();
-            AWGN_Channel chan(N0 / 2);
-            BPSK mod;
-            bvec coded;
-            p.encode(bitsin, coded);
-            vec s = mod.modulate_bits(coded);
-            vec x = chan(s);
-            vec llr = mod.demodulate_soft_bits(x, N0);
-            bvec output;
-            p.decode(llr, output);
-            ber.clear();
-            ber.count(bitsin, output);
-            bit_error_rate[m * length + i] = ber.get_errorrate();
-        }
+    unsigned char m = 0;
+    if (argc >= 2)
+        m = atoi(argv[1]);
+    bvec bitsin;
+    std::ofstream f;
+    if (m == 0) {
+        p.set_polar_decoder(POLAR_SC);
+        bitsin = randb(BLK_LEN * 1000);
+        bler.set_blocksize(BLK_LEN);
+        f.open("sc.dat");
     }
-    std::ofstream f("test.dat");
+    if (m == 1) {
+        p.set_polar_decoder(POLAR_SCL);
+        bitsin = randb(BLK_LEN * 1000);
+        p.set_scl_size(LIST_SIZE);
+        bler.set_blocksize(BLK_LEN);
+        f.open("scl.dat");
+    }
+    if (m == 2) {
+        p.set_polar_decoder(POLAR_CASCL);
+        p.set_crc_code(CRC, CRC_LEN);
+        p.set_scl_size(LIST_SIZE);
+        bitsin = randb((BLK_LEN / 2 - CRC_LEN) * 2000);
+        bler.set_blocksize(BLK_LEN - CRC_LEN);
+        f.open("cascl.dat");
+    }
     for (int i = 0; i < length; ++i) {
-        f << setw(4) << EbN0db(i) << setw(12) << bit_error_rate(i) << setw(12) << bit_error_rate(i + length) << endl;
+        double N0 = pow(10.0, -EbN0db[i] / 10.0) / p.get_rate();
+        AWGN_Channel chan(N0 / 2);
+        BPSK mod;
+        bvec coded;
+        p.encode(bitsin, coded);
+        vec s = mod.modulate_bits(coded);
+        vec x = chan(s);
+        vec llr = mod.demodulate_soft_bits(x, N0);
+        bvec output;
+        p.decode(llr, output);
+        ber.clear();
+        ber.count(bitsin, output);
+        bit_erate[i] = ber.get_errorrate();
+        bler.clear();
+        bler.count(bitsin, output);
+        blk_erate[i] = bler.get_errorrate();
+    }
+    for (int i = 0; i < length; ++i) {
+        f << setw(4) << EbN0db(i) << setw(12) << bit_erate(i) << setw(12) << blk_erate(i) << endl;
     }
     f.close();
     return 0;
--- polar/test.gp
+++ polar/test.gp
@@ -1,9 +1,17 @@
 set grid
+set term wxt size 600,300
+set multiplot layout 1,2
 set logscale y
 set xlabel "Eb/N0[db]"
 set ylabel "BER"
 set title "BER for Polar code"
 set format y "10^{%L}"
-plot "test.dat" using 1:2 w lp pt 5 title "SC",\
-     "test.dat" using 1:3 w lp pt 7 title "SCL-8"
+plot "sc.dat" using 1:2 w lp pt 5 title "SC",\
+     "scl.dat" using 1:2 w lp pt 7 title "SCL-16",\
+     "cascl.dat" using 1:2 w lp pt 9 title "CASCL-16"
+set ylabel "FER"
+set title "FER for Polar code"
+plot "sc.dat" using 1:3 w lp pt 5 title "SC",\
+     "scl.dat" using 1:3 w lp pt 7 title "SCL-16",\
+     "cascl.dat" using 1:3 w lp pt 9 title "CASCL-16"
 pause mouse any "Hit any key to exit\r\n"
