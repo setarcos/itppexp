84248271a46156e9c831cb066aa17c1b9d384ab9
Pluto Yang <yangyj.ee@gmail.com>
2022-04-22 17:14:46 +0800

    Better command line

--- polar/main.cpp
+++ polar/main.cpp
@@ -2,59 +2,131 @@
 #include <itpp/itcomm.h>
 #include <iostream>
 #include <iomanip>
+#include <getopt.h>
+#include <string.h>
 #include "polar.h"
 
 using namespace itpp;
 using std::endl;
 using std::cout;
 using std::setw;
+using std::cerr;
 
 #define BLK_LEN 2048
-#define CRC "CCITT-16"
-#define CRC_LEN 16
 #define LIST_SIZE 16
 #define RUN_SIZE 1000
 
 int main(int argc, char *argv[])
 {
-    Polar p(BLK_LEN, BLK_LEN / 2);
-    p.gen_frozen_ga(0.79433);
+    const char *optstr = "hn:k:c:p:l:";
+    int n = BLK_LEN;
+    int k = 0;
+    int c = 0;
+    char *crc = nullptr; // initialize later
+    char *pac = nullptr;
+    int l = LIST_SIZE;
+    int f = 0;
+    int o;
+    char *filename = nullptr;
+    while ((o = getopt(argc, argv, "hn:k:c:C:p:l:f:o:")) != -1) {
+        switch (o) {
+            case 'h':
+                cout << argv[0] << ": polar(pac) code simulation." << endl
+                    << "\t-n [block_length]" << endl
+                    << "\t-k [info_length]" << endl
+                    << "\t-c [CRC_length]" << endl
+                    << "\t-C [CRC standard polynomials]" << endl
+                    << "\t-f [0 for bec, 1 for ga, 2 for rm]" << endl
+                    << "\t-p [PAC polynomial]" << endl
+                    << "\t-l [list size]" << endl;
+                return 0;
+            case 'n':
+                n = atoi(optarg);
+                break;
+            case 'k':
+                k = atoi(optarg);
+                break;
+            case 'c':
+                c = atoi(optarg);
+                break;
+            case 'C':
+                crc = (char*) malloc(strlen(optarg) + 1);
+                strcpy(crc, optarg);
+                break;
+            case 'p':
+                pac = (char*) malloc(strlen(optarg) + 1);
+                strcpy(pac, optarg);
+                break;
+            case 'o':
+                filename = (char*) malloc(strlen(optarg) + 1);
+                strcpy(filename, optarg);
+                break;
+            case 'l':
+                l = atoi(optarg);
+                break;
+            case 'f':
+                f = atoi(optarg);
+                break;
+        }
+    }
+
+    if (k == 0) k = n / 2;
+    if ((c > 0) && (!crc)) {
+        cerr << "CRC polynomials needed." << endl;
+        return 1;
+    }
+    if (!filename) {
+        cerr << "Output filename needed." << endl;
+        return 1;
+    }
+    cout << "Block length " << n << "; Info length " << k << "; CRC length " << c << "; List size " << l <<  endl;
+    Polar p(n, k);
+    switch (f) {
+        case 1:
+            p.gen_frozen_ga(0.79433);
+            break;
+        case 2:
+            p.gen_frozen_rm();
+            break;
+        default:
+            p.gen_frozen_bec(0.32);
+    }
     BERC ber;
     BLERC bler;
+    Real_Timer tt;
     vec EbN0db = "1:0.25:3";
     vec bit_erate;
     vec blk_erate;
     int length = EbN0db.length();
     bit_erate.set_size(length);
     blk_erate.set_size(length);
+    bler.set_blocksize(k - c);
     RNG_randomize();
-    unsigned char m = 0;
-    if (argc >= 2)
-        m = atoi(argv[1]);
     bvec bitsin;
-    std::ofstream f;
-    int runsize = BLK_LEN / 2 * RUN_SIZE;
-    if (m == 0) {
+    std::ofstream fn;
+    int runsize = (k - c) * RUN_SIZE;
+    if (l == 0) // SC Decorder
         p.set_polar_decoder(POLAR_SC);
-        bler.set_blocksize(BLK_LEN / 2);
-        f.open("sc.dat");
+    else
+        p.set_scl_size(l);
+
+    if (pac) {
+        p.set_polar_decoder(PAC_SCL);
+        p.set_pac_generator(pac);
+    } else {
+        if (c > 0)
+            p.set_polar_decoder(POLAR_CASCL);
+        else
+            if (l > 0) p.set_polar_decoder(POLAR_SCL);
     }
-    if (m == 1) {
-        p.set_polar_decoder(POLAR_SCL);
-        p.set_scl_size(LIST_SIZE);
-        bler.set_blocksize(BLK_LEN / 2);
-        f.open("scl.dat");
-    }
-    if (m == 2) {
-        p.set_polar_decoder(POLAR_CASCL);
-        p.set_crc_code(CRC, CRC_LEN);
-        p.set_scl_size(LIST_SIZE);
-        bler.set_blocksize(BLK_LEN / 2 - CRC_LEN);
-        f.open("cascl.dat");
-        runsize = (BLK_LEN / 2 - CRC_LEN) * RUN_SIZE;
-    }
+
+    if (c > 0)
+        p.set_crc_code(crc, c);
+
     ber.clear();
     bler.clear();
+    tt.tic();
+    cout << " EbN0   ErrorRate   ElapsedTime" << endl;
     for (int i = 0; i < length;) {
         bitsin =randb(runsize);
         double N0 = pow(10.0, -EbN0db[i] / 10.0) / p.get_rate();
@@ -72,14 +144,20 @@ int main(int argc, char *argv[])
         if ((bler.get_errors() > 100) || (bler.get_total_blocks() > 50000)) {
             bit_erate[i] = ber.get_errorrate();
             blk_erate[i] = bler.get_errorrate();
+            cout << setw(5) << EbN0db(i) << setw(12) << bit_erate[i] << setw(12) << tt.toc() << " seconds" << endl;
+            tt.tic();
             i++;
             ber.clear();
             bler.clear();
         }
     }
+    fn.open(filename);
+    if (pac) fn << "#PAC(" << pac << "),";
+    else fn << "#Polar,";
+    fn << "Block length " << n << "; Info length " << k << "; CRC length " << c << "; List size " << l <<  endl;
     for (int i = 0; i < length; ++i) {
-        f << setw(4) << EbN0db(i) << setw(12) << bit_erate(i) << setw(12) << blk_erate(i) << endl;
+        fn << setw(4) << EbN0db(i) << setw(12) << bit_erate(i) << setw(12) << blk_erate(i) << endl;
     }
-    f.close();
+    fn.close();
     return 0;
 }
--- polar/test.gp
+++ polar/test.gp
@@ -1,3 +1,6 @@
+# sc.dat:   ./main -n 2048 -f 1 -l 0 -o sc.dat
+# scl.dat:  ./main -n 2048 -f 1 -l 32 -o scl.dat
+# cascl.dat:./main -n 2048 -f 1 -l 32 -c 16 -C CCITT-16 -o cascl.dat
 set grid
 set term qt size 900,450
 set multiplot layout 1,2
@@ -7,11 +10,11 @@ set ylabel "BER"
 set title "BER for Polar code"
 set format y "10^{%L}"
 plot "sc.dat" using 1:2 w lp pt 5 title "SC",\
-     "scl.dat" using 1:2 w lp pt 7 title "SCL-16",\
-     "cascl.dat" using 1:2 w lp pt 9 title "CASCL-16"
+     "scl.dat" using 1:2 w lp pt 7 title "SCL-32",\
+     "cascl.dat" using 1:2 w lp pt 9 title "CASCL-32"
 set ylabel "FER"
 set title "FER for Polar code"
 plot "sc.dat" using 1:3 w lp pt 5 title "SC",\
-     "scl.dat" using 1:3 w lp pt 7 title "SCL-16",\
-     "cascl.dat" using 1:3 w lp pt 9 title "CASCL-16"
+     "scl.dat" using 1:3 w lp pt 7 title "SCL-32",\
+     "cascl.dat" using 1:3 w lp pt 9 title "CASCL-32"
 pause mouse any "Hit any key to exit\r\n"
